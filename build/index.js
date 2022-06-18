1916c7858d876769cf2315bc94d1201f2aa4cbf7
garguelles <gerard.arguelles@gmail.com>
2022-06-18 14:53:38 +0800

    src

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello world!");
+Object.defineProperty(exports, "__esModule", { value: true });
+const types_1 = require("./src/types");
+const matrix = [
+    [0, 1, 2, 3, 4],
+    [0, 1, 2, 3, 4],
+    [0, 1, 2, 3, 4],
+    [0, 1, 2, 3, 4],
+    [0, 1, 2, 3, 4],
+];
+console.log("direction", types_1.Direction, matrix);
