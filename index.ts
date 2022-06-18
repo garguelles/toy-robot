a258edd4a775473be125683f529cd226d6dc33ea
garguelles <gerard.arguelles@gmail.com>
2022-06-18 14:53:50 +0800

    tsc config

---
+++ index.ts
@@ -0,0 +1,13 @@
+import { Direction } from "./src/types";
+
+const matrix = [
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+];
+
+console.log("direction", Direction, matrix);
+
+
