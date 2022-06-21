dd9b1236c347f0beda5abdf9b5f3e88225f4bfc4
garguelles <gerard.arguelles@gmail.com>
2022-06-21 15:34:44 +0800

    move validation test

--- tests/commmands.test.ts
+++ tests/commmands.test.ts
@@ -2,11 +2,13 @@ const {place, move} = require("../src/commands");
 const {DirectionEnum} = require("../src/types");
 const {InvalidLocationError} = require("../src/exceptions");
 
+const MATRIX_SIZE = { xSize: 5, ySize: 5 };
+
 test("can place the robot", () => {
   const state = {
     location: null,
     facing: null,
-    matrixSize: { xSize: 5, ySize: 5 },
+    matrixSize: MATRIX_SIZE,
   };
   const coordinates = { x: 1, y: 0 };
   const facing = DirectionEnum.NORTH;
@@ -22,7 +24,7 @@ test("cannot place the robot outside the table", () => {
   const state = {
     location: null,
     facing: null,
-    matrixSize: { xSize: 5, ySize: 5 },
+    matrixSize: MATRIX_SIZE,
   };
   const coordinates = { x: 1, y: 6 };
   const facing = DirectionEnum.NORTH;
@@ -38,7 +40,7 @@ test("can move a robot", () => {
   const state = {
     location: { x: 0, y: 0},
     facing: DirectionEnum.NORTH,
-    matrixSize: { xSize: 5, ySize: 5 },
+    matrixSize: MATRIX_SIZE,
   };
 
   const expected = {
@@ -48,3 +50,14 @@ test("can move a robot", () => {
 
   expect(move(state, state.location, state.facing)).toEqual(expected);
 });
+
+test("ensure robot does not fall of the edge", () => {
+  const state = {
+    location: { x: 4, y: 0},
+    facing: DirectionEnum.EAST,
+    matrixSize: MATRIX_SIZE,
+  };
+
+  expect(() => move(state, state.location, state.facing)).toThrow(InvalidLocationError);
+});
+
