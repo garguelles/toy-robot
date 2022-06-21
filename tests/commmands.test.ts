32c4c3a1cc1c3be7823d31560d6702b44f5faed6
garguelles <gerard.arguelles@gmail.com>
2022-06-21 16:21:03 +0800

    test move placement

--- tests/commmands.test.ts
+++ tests/commmands.test.ts
@@ -1,6 +1,8 @@
-const {place, move, turn} = require("../src/commands");
-const {DirectionEnum, TurnEnum} = require("../src/types");
-const {InvalidLocationError} = require("../src/exceptions");
+import { RobotNotPlacedError } from "../src/exceptions";
+
+const { place, move, turn } = require("../src/commands");
+const { DirectionEnum, TurnEnum } = require("../src/types");
+const { InvalidLocationError } = require("../src/exceptions");
 
 const MATRIX_SIZE = { xSize: 5, ySize: 5 };
 
@@ -28,17 +30,13 @@ test("cannot place the robot outside the table", () => {
   };
   const coordinates = { x: 1, y: 6 };
   const facing = DirectionEnum.NORTH;
-  const expected = {
-    location: coordinates,
-    facing,
-  };
 
   expect(() => place(state, coordinates, facing)).toThrow(InvalidLocationError);
 });
 
 test("can move a robot", () => {
   const state = {
-    location: { x: 0, y: 0},
+    location: { x: 0, y: 0 },
     facing: DirectionEnum.NORTH,
     matrixSize: MATRIX_SIZE,
   };
@@ -51,9 +49,19 @@ test("can move a robot", () => {
   expect(move(state, state.location, state.facing)).toEqual(expected);
 });
 
+test("cannot move a robot without calling place.", () => {
+  const state = {
+    location: null,
+    facing: null,
+    matrixSize: MATRIX_SIZE,
+  };
+
+  expect(() => move(state, state.location, state.facing)).toThrow(RobotNotPlacedError);
+});
+
 test("ensure robot does not fall of the edge", () => {
   const state = {
-    location: { x: 4, y: 0},
+    location: { x: 4, y: 0 },
     facing: DirectionEnum.EAST,
     matrixSize: MATRIX_SIZE,
   };
@@ -63,7 +71,7 @@ test("ensure robot does not fall of the edge", () => {
 
 test("can turn robot", () => {
   const state = {
-    location: { x: 2, y: 1},
+    location: { x: 2, y: 1 },
     facing: DirectionEnum.EAST,
     matrixSize: MATRIX_SIZE,
   };
